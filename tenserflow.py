f2d44821488de3c5070359d568569b622a1efe62
Panda0831 <fanambymanjaka9@gmail.com>
2025-03-13 21:02:09 +0300

    Add files via upload
    pffff

---
+++ tenserflow.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+model.add(tf.keras.layers.Dropout(0.2))
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test, y_test)
